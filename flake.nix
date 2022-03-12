0ac60863d812cbeb24917e5bc1a5597ef38e7f01
hugosenari <hugosenari@gmail.com>
2022-03-12 03:40:56 -0300

    chore: re index toktok

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Generic tokenizer written in Nim language 👑 Powered by Nim's Macros'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."toktok-main".url = "path:./main";
+  inputs."toktok-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."toktok-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Generic tokenizer written in Nim language 👑 Powered by Nim's Macros'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-toktok-main.flake = false;
+  inputs.src-toktok-main.owner = "openpeep";
+  inputs.src-toktok-main.ref   = "refs/heads/main";
+  inputs.src-toktok-main.repo  = "toktok";
+  inputs.src-toktok-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-toktok-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-toktok-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
